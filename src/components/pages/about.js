497a3e961a868ad42f3386d2320a92ef91aab936
jakecapra <jacob.capra@mymail.champlain.edu>
2020-01-20 15:21:00 -0500

    starter code for site (using react). No content added, only nav bar right now

---
+++ src/components/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a paragraloihjnas look at m,e typing, wow im so special</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
