4ca8c138a794715c9e289b7c4f347b464aca718a
jakecapra <jacob.capra@mymail.champlain.edu>
2020-01-20 17:16:16 -0500

    created an about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
+import About from './components/pages/about';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <div className="App">
         <div className="container">
           <Header />
+
+        <Route path="/About" component={About} />
         </div>
       </div>
     </Router>
