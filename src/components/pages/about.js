9048e0671e4453d216db5e62abb1bf85fabbd4a6
Nathan DiMauro <nathan.dimauro@mymail.champlain.edu>
2020-01-27 23:35:58 -0500

    added some work into rendering the pets on the screen

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import pf from '../pf.js';
 
 function About() {
+    var img;
+    pf.animal.search({age: "baby", gender: "female", status: "adoptable"})
+    .then(resp =>{
+        img=resp.data.animals[0].photos[0].large
+        console.log(img)
+    });
     return (
         <React.Fragment>
             <h1>About</h1>
             <p>This is a paragraloihjnas look at m,e typing, wow im so special</p>
+            <img src={img} alt="this is a dog" />
         </React.Fragment>
     )
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,13 +3,17 @@ import dog from './../images/dog.jpg';
 import pf from '../pf.js';
 
 function Home() {
-    pf.animal.search({type: "Dog"})
+    var img;
+   // pf.animal.search({type: "Dog"})
+    pf.animal.search({age: "baby", gender: "female", status: "adoptable"})
     .then(resp =>{
         console.log("starting search...");
         console.log("Search results", resp.data.animals)
         console.log("First animal results", resp.data.animals[0])
         console.log("First animal name: ", resp.data.animals[0].name)
         console.log("...search complete");
+        img=resp.data.animals[0].photos[0].large
+        console.log(img)
     });
     return (
         <img src={dog} alt="this is a dog" />
