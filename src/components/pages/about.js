a09c897fd8c264a40968cd6abb131c93d09875ce
Nathan DiMauro <nathan.dimauro@mymail.champlain.edu>
2020-01-29 13:57:42 -0500

    made the getImg function

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import miss from './../images/no-Photo.jpg';
 import pf from '../pf.js';
 
-function About() {
+/*get an image from the API*/
+function getImg()
+{
+    /* declare variables */
+    var len;
     var img;
-    pf.animal.search({age: "baby", gender: "female", status: "adoptable"})
+
+    /*API Search*/
+    pf.animal.search({type: "dog", status: "adoptable"})
     .then(resp =>{
-        img=resp.data.animals[0].photos[0].large
-        console.log(img)
-    });
+
+        /* output photots array to console for testing */
+        console.log(resp.data.animals[0].photos);
+
+        /* get length */
+        len=resp.data.animals[0].photos.length;
+        console.log("Photo Array Length: ", len);
+
+        /*if length > 1 get img*/
+        if (len > 0)
+        {
+            img=resp.data.animals[0].photos[0].medium
+        }
+
+        /*else set img to missing image*/
+        else
+        {
+            img=miss;
+        }
+    })
+    /*once the api search is done*/
+    .then(() => {
+        /*return img*/
+        console.log(img);
+        return img;
+    })
+};
+
+function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is a paragraloihjnas look at m,e typing, wow im so special</p>
-            <img src={img} alt="this is a dog" />
-        </React.Fragment>
+            <p>This dog is adoptable</p>
+            <img src={getImg()} alt="adoptable dog" />
+            </React.Fragment>
     )
 }
-
 
 export default About;
