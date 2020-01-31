1b31d43a3ddf30f81933f6884de87df710dcf03e
Nathan DiMauro <nathan.dimauro@mymail.champlain.edu>
2020-01-30 20:27:50 -0500

    render an image from the API on the about page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,17 +3,6 @@ import dog from './../images/dog.jpg';
 import pf from '../pf.js';
 
 function Home() {
-    var img;
-    pf.animal.search({type: "Dog"})
-    .then(resp =>{
-        console.log("starting search...");
-        console.log("Search results", resp.data.animals)
-        console.log("First animal results", resp.data.animals[0])
-        console.log("First animal name: ", resp.data.animals[0].name)
-        console.log("...search complete");
-        img=resp.data.animals[0].photos[0].large
-        console.log(img)
-    });
     return (
         <img src={dog} alt="this is a dog" />
     )
