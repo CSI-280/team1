b616e9897fb3d734d201b832f0254d3f7154224e
Mike <michael.bamford@mymail.champlain.edu>
2020-02-05 19:12:51 -0500

    added searchresults

--- src/components/pages/searchresults.js
+++ src/components/pages/searchresults.js
@@ -1,28 +1,52 @@
 import React from 'react'
 import pf from '../pf.js';
-import Header from '../header.js'
+import { render } from '@testing-library/react';
+//import Header from '../header.js'
 
 function searchResults() {
 	let userInput = document.getElementById("search")
+
 	
 	pf.animal.search({type:	userInput})
     .then(resp =>{
-        console.log("Search results", resp.data.animals);
+		console.log("Search results", resp.data.animals);
 		
-		document.getElementById("animalName").innerHTML = resp.data.animals[0].name
-		document.getElementById("animalType").innerHTML = resp.data.animals[0].type
-		document.getElementById("animalGender").innerHTML = resp.data.animals[0].gender
-		document.getElementById("animalPicture").src = resp.data.animals[0].photos.large
-    });
+		var i;
+
+		for (i = 0; i < 20; i++)
+		{
+			document.getElementById("animalName" + i).innerHTML = resp.data.animals[i].name
+			document.getElementById("animalType" + i).innerHTML = resp.data.animals[i].type
+			document.getElementById("animalGender" + i).innerHTML = resp.data.animals[i].gender
+			document.getElementById("animalPicture" + i).src = resp.data.animals[i].photos.large
+		}
+	});
 	
+	let htmlanimals = [];
+
+	var i;
+
+	for (i = 0; i < 20; i++)
+	{
+		htmlanimals.push("<h2 id=\"animalName" + i + "\"></h2>");
+		htmlanimals.push("<img id=\"animalPicture" + i + "\" src=''></div>");
+		htmlanimals.push("<div id=\"animalType" + i + "\"></div>");
+		htmlanimals.push("<div id=\"animalGender" + i + "\"></div>")
+	}
+
 	return (
 		<div>
-			<div id="animalType"></div>
-			<div id="animalGender"></div>
-			<div id="animalName"></div>
-			<img id="animalPicture" src=""/>
+			{htmlanimals.map(htmlanimal => <div style={animal}>{htmlanimal}</div>)}
 		</div>
 	)
 }
 
+const animal = {
+	float: 'left',
+	padding: '10px',
+	backgroundColor: '#A9A9A9',
+	border: '1px solid black',
+	width: '250px',
+	height: '250px'
+}
 export default searchResults
