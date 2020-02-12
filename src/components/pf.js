491dda85e0cfa3bf5ab6d1965e34c1b231476f09
Mike <michael.bamford@mymail.champlain.edu>
2020-02-11 20:14:17 -0500

    its working and styled

--- src/components/pf.js
+++ src/components/pf.js
@@ -1,5 +1,5 @@
 var petfinder = require("@petfinder/petfinder-js");
 
-export const pf = new petfinder.Client({apiKey: "HB4E0LPBodtgXJlBVOYvZSDaxgGSCA7Li7Eq6tqb6uVDRfzAp4", secret: "ACCq1YmJ2XzZxT6WrvbSU9voepnbCllw0NmSF363"});
+export const pf = new petfinder.Client({apiKey: "L7xN9DjGWgJENiTju1kRMvoFeuxtDVPDXJhLhzCbKHP8uXgCEZ", secret: "mCJgTIn4QFV0pBoQXsNz0YPPKQzbraELqfonphtm"});
 
 export default pf
--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import pf from './pf.js';
 import miss from './images/no-Photo.jpg';
-import './styles/adoptStyle.css';
+import './styles/searchStyle.css'
 
 //https://dev.to/sage911/how-to-write-a-search-component-with-suggestions-in-react-d20
 // also used a lot of nates adopt page code
@@ -16,9 +16,9 @@ class Search extends React.Component {
             image: miss,
             name: "Loading...", 
             gender: "Loading...", 
-            age: "Loading...",
-            breed: "Loading...",
-            link: "Loading...",
+            age: "",
+            breed: "",
+            link: "",
 		}
 	}
 
@@ -26,13 +26,17 @@ class Search extends React.Component {
 		this.setState({
 			query: event.target.value
 		})
+		console.log(this.state.query)
+		//this.getInfo();
 	}
 
-	getInfo() {
+	getInfo = () => {
 
 		/* declare variables */
 		let len;
 		
+		console.log("Test")
+
 		pf.animal.search({type: this.state.query})
         .then(resp =>{
 			this.setState({
@@ -51,21 +55,30 @@ class Search extends React.Component {
 			{
 				//set image variable
 				this.setState({
-					image: resp.data.animals[this.props.index].photos[0].small
+					image: resp.data.animals[this.props.index].photos[0].medium
 				})
 			}
+			else
+			{
+				this.setState({
+					image: miss
+				})
+			}
+
 		})
 	}
 
 	render() {
 		return (
 			<div>
-				<input id="search" type="text" value={this.state.query} onChange={event => this.handleChange(event)} placeholder="Search here..." />
-				<input onClick={this.getInfo()} type="submit" value="Search"/>
-				<div onClick={() => window.open(this.state.link)} onMouseOver={() => this.style="background-color: #292c34;"}>
+				<div class="inputWrapper">
+					<input class="search" type="text" value={this.state.query} onChange={event => this.handleChange(event)} placeholder="Search here..." />
+					<button class="button" onClick={() => this.getInfo()}>Search</button>
+				</div>
+				<div class="searchresults" onClick={() => window.open(this.state.link)} onMouseOver={() => this.style="background-color: #292c34;"}>
 					<a href={this.state.link} target="_blank" rel="noopener noreferrer"></a>
-					<h3>{this.state.name} is adoptable</h3>
-					<img src={this.state.image} alt="Adoptable Dog" width="100" height="100"/>
+					<h3>{this.state.name}</h3>
+					<img src={this.state.image} alt="Adoptable Dog" width="400" height="500"/>
 					<p>{this.state.age} {this.state.gender} {this.state.breed}</p>
 				</div>
 			</div>
