4717709cb438e7d93ed29bbd38f0ba07989ee951
jakecapra <jacob.capra@mymail.champlain.edu>
2020-02-14 14:55:56 -0500

    did a little styling

--- src/components/pet.js
+++ src/components/pet.js
@@ -25,63 +25,56 @@ class Pet extends React.Component {
             link: "Loading...",
             display: false,
             type: "",
+            id: "",
+            prevId: 1,
         }
 
-    }
-    //mouseOver/Leave are for the button
-    mouseOver = (e) => {
-        e.target.style.backgroundColor = '#7D7D7D';
-    }
-    mouseLeave = (e) => {
-        e.target.style.backgroundColor = '#333';
-
-    }
-
-    linkCursor = (e) => {
-        e.target.style.cursor = 'pointer';
     }
 
     /*get an image from the API*/
     getPet(aType)
     {
         this.display = true;
+        this.setState({prevId: this.state.id, })
+        console.log(this.state.prevId + "and" + this.state.id);
         
         
         
         /* declare variables */
         let len;
 
-        pf.animal.search({type: aType, status: "adoptable"})
-        .then(resp =>{
+            pf.animal.search({type: aType, status: "adoptable"})
+            .then(resp =>{
 
-            /* output photots array to console for testing */
-            console.log(resp.data.animals[this.props.index]);
-            
-            //set variables 
-            this.setState({
-                name: resp.data.animals[this.props.index].name, 
-                gender: resp.data.animals[this.props.index].gender, 
-                age: resp.data.animals[this.props.index].age,
-                breed: resp.data.animals[this.props.index].breeds.primary,
-                link: resp.data.animals[this.props.index].url,
-                type: aType,
-            })
-
-            /* get length of photos array */
-            len=resp.data.animals[this.props.index].photos.length;
-
-            /*if length > 1 get img*/
-            if (len > 0)
-            {
-                //set image variable
+                /* output photots array to console for testing */
+                console.log(resp.data.animals[this.props.index]);
+                
+                //set variables 
                 this.setState({
-                    image: resp.data.animals[this.props.index].photos[0].medium
+                    name: resp.data.animals[this.props.index].name, 
+                    gender: resp.data.animals[this.props.index].gender, 
+                    age: resp.data.animals[this.props.index].age,
+                    breed: resp.data.animals[this.props.index].breeds.primary,
+                    link: resp.data.animals[this.props.index].url,
+                    type: aType,
+                    id: resp.data.animals[this.props.index].id,
                 })
-            }
-            else {
-                this.setState({image: this.state.images[Math.floor(Math.random() * this.state.images.length)],});
-            }
-        })
+
+                /* get length of photos array */
+                len=resp.data.animals[this.props.index].photos.length;
+
+                /*if length > 1 get img*/
+                if (len > 0)
+                {
+                    //set image variable
+                    this.setState({
+                        image: resp.data.animals[this.props.index].photos[0].medium
+                    })
+                }
+                else {
+                    this.setState({image: this.state.images[Math.floor(Math.random() * this.state.images.length)],});
+                }
+            })
     };
 
     render() {
@@ -90,24 +83,26 @@ class Pet extends React.Component {
         //If display, then show the animal
         if (disp) {
             content = 
-            <React.Fragment>
-                <h1 style={nameStyle} onClick={() => window.open(this.state.link)} onMouseOver={this.linkCursor}>{this.state.name}</h1>
-                <h3 style={infoStyle}>{this.state.type} - {this.state.breed}</h3>
-                <h3 style={infoStyle}>{this.state.gender} - {this.state.age}</h3>
-                <img style={imgStyle} src = {this.state.image} alt={this.state.type} onClick={() => window.open(this.state.link)} onMouseOver={this.linkCursor}/>
-            </React.Fragment>;
+                <div className="col" onClick={() => window.open(this.state.link)} onMouseOver={() => this.style="background-color: #292c34;"}>
+                    <a href={this.state.link} target="_blank" rel="noopener"></a>
+                    <h3>{this.state.name} is adoptable</h3>
+                    <img src={this.state.image} alt="Adoptable Dog" width="400" height="500"/>
+                    <p>{this.state.age} {this.state.gender} {this.state.breed}</p>
+                </div>
         }
 
         return (
             <React.Fragment>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Dog")}}>Dog</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Cat")}}>Cat</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Rabbit")}}>Rabbit</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Small & Furry")}}>Small and Furry</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Horse")}}>Horse</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Bird")}}>Bird</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Scales, Fins & Others")}}>Scales, Fins and Others</button>
-                <button style={buttonStyle} onClick={() =>{this.getPet("Barnyard")}}>Barnyard</button>
+                <div>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Dog")}}>Dog</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Cat")}}>Cat</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Rabbit")}}>Rabbit</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Small & Furry")}}>Small and Furry</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Horse")}}>Horse</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Bird")}}>Bird</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Scales, Fins & Others")}}>Scales, Fins and Others</button>
+                    <button style={buttonStyle} onClick={() =>{this.getPet("Barnyard")}}>Barnyard</button>
+                </div>
                 {content}
             </React.Fragment>
         );
@@ -125,6 +120,10 @@ const imgStyle = {
     width: '30%',
     height: "auto",
     borderRadius: '7%',
+}
+
+const fragStyle = {
+    clear: 'both',
 }
 
 const buttonStyle = {
