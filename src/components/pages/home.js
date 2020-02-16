38533cecdf571de5b5e9239b5b9b1bab95eb2a48
Nathan DiMauro <nathan.dimauro@mymail.champlain.edu>
2020-02-15 19:18:24 -0500

    added right side bar of adoptable dogs

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import dog from './../images/dog.jpg';
+import Pet from '../pet.js';
+import '../styles/homeStyle.css';
+
 
 function Home() {
     return (
-        <img style={dogStyle} src={dog} alt="this is a dog" />
+        <container>
+            <div id="left">
+                <img style={dogStyle} src={dog} alt="this is a dog" wdith="300" height="350"/>
+            </div>
+            <div id="right">
+                <h3>Aviable Dogs</h3>
+                <Pet index="0"/>
+                <Pet index="1"/>
+                <Pet index="2"/>
+                <Pet index="3"/>
+            </div>
+        </container>
     )
 }
 
