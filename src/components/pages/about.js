4f63f0f197b782f00d916d3ff3384c2fd17446f3
Mike <michael.bamford@mymail.champlain.edu>
2020-02-17 21:59:24 -0500

    added content to the about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import '../styles/aboutStyle.css'
+import petfinder from '../images/petfinder.png'
+import reactpic from '../images/reactpic.png'
 
 function About() {
     return (
         <React.Fragment>
-            <h1>About</h1>
-            <p>We are a team of indepentdent developers.</p>
+                <h1>About</h1>
+                <table>
+                    <tr>
+                        <td class="general">
+                            <h2>Developers</h2>
+                            <ul>
+                                <li>Michael Bamford</li>
+                                <li>Nate DiMauro</li>
+                                <li>Jake Capra</li>
+                                <li>Philip Bell</li>
+                            </ul>
+                            <p>We are all from Champlain College in the class Innovation II: Open Source Software Dev. The teacher is Chris Bendel.</p>
+                        </td>
+                        <td class="general">
+                            <h2>Goal</h2>
+                            <p>The goal of the project is to simualate a real world develpment team.</p>
+                        </td>
+                        <td class="general">
+                            <h2>Resources</h2>
+                            <p>This site uses the <a href="https://www.petfinder.com/developers/v2/docs/">Petfinder API</a> and <a href="https://reactjs.org/">React Framework</a>.</p>
+                        </td>
+                    </tr>
+                </table>
+                <img class="image" src={petfinder} alt="Petfinder Logo"></img>
+                <img class="image" src={reactpic} alt="React Logo"></img>
         </React.Fragment>
     )
 }
